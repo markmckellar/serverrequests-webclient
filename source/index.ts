d36396a39caa34aa3d19fe96998b536d0adbf908
Mark McKellar <mm@mygn.myriad.com>
2019-04-23 12:53:06 -0600

    took out testing client

--- source/index.ts
+++ source/index.ts
@@ -4,4 +4,4 @@ export { UserManagement } from "./common/user/usermanagement";
 export { UserManagementOAuth2 } from "./common/user/usermanagementoauh2";
 export { ServerRequestsOAuth2 } from "./common/server/serverrequestsoauth2";
 export { BaseApp } from "./common/ui/baseapp";
-import { VersionBase } from "./common/server/versionbase"; 
+export { VersionBase } from "./common/server/versionbase"; 
